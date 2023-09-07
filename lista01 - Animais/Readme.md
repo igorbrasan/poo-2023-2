40cc4a51175db4019c88b96d06b1012ca67e6a24
igor <igorbraga.rsantos@gmail.com>
2023-09-07 18:04:41 -0300

    finished animal.java

---
+++ lista01 - Animais/Readme.md
@@ -0,0 +1,6 @@
+4. Criar o modelo e implementar em Java uma classe que represente animais. A classe
+   possui os atributos tipo (mamífero, anfíbio, ave), nome, idade. Esta classe deve possuir o
+   seguinte método:
+   a) Método para calcular a quantidade de alimento consumido por dia, sabendo- se que a
+   cada ano de vida um mamífero come 2 quilos/dia, uma ave come 100g/dia um anfíbio
+   20g/dia.
--- lista01 - Animais/src/Animal.java
+++ lista01 - Animais/src/Animal.java
@@ -1,2 +1,49 @@
-package PACKAGE_NAME;public class Animal {
+public class Animal {
+    private String tipo, nome;
+    private int idade;
+
+    public Animal(String tipo, String nome, int idade){
+        this.idade = idade;
+        this.tipo = tipo;
+        this.nome = nome;
+    }
+
+    public int consumoDiarioEmGramas(){
+        int consumo;
+        if(tipo == "ave"){
+            consumo = 100;
+        }else if (tipo == "mamifero"){
+            consumo = 2000;
+        }else if (tipo == "anfibio"){
+            consumo = 20;
+        }else{
+            throw  new IllegalArgumentException("Você deve escolher um dos tipos: mamifero, ave, anfibio");
+        }
+        return consumo;
+
+    }
+    //Getters and setters
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
 }
