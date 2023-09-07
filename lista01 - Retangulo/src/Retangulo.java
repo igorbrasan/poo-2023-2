c1f209cdab54e29435c45dc7198642fbd6543f72
igor <igorbraga.rsantos@gmail.com>
2023-09-07 17:26:50 -0300

    Exercicio Retângulo

---
+++ lista01 - Retangulo/src/Retangulo.java
@@ -0,0 +1,48 @@
+public class Retangulo {
+    private double comprimento, largura;
+     public Retangulo(double comprimento, double largura){
+         if (comprimento <= 0 || largura <= 0) {
+             throw new IllegalArgumentException("Comprimento e largura devem ser maiores que 0.");
+         }
+         this.comprimento = comprimento;
+         this.largura = largura;
+     }
+     public Retangulo(double lado){
+         if (lado <= 0) {
+             throw new IllegalArgumentException("Lado deve ser maior que 0.");
+         }
+         this.comprimento = lado;
+         this.largura = lado;
+     }
+     public double area(){
+         return comprimento * largura;
+     }
+
+     public double perimetro(){
+     return 2 * (comprimento + largura);
+     }
+
+
+     //Getters e setters
+    public double getComprimento() {
+        return comprimento;
+    }
+
+    public void setComprimento(double comprimento) {
+        if (comprimento <= 0) {
+            throw new IllegalArgumentException("Lado deve ser maior que 0.");
+        }
+        this.comprimento = comprimento;
+    }
+
+    public double getLargura () {
+            return largura;
+        }
+
+    public void setLargura ( double largura){
+        if (largura <= 0) {
+            throw new IllegalArgumentException("Lado deve ser maior que 0.");
+        }
+           this.largura = largura;
+    }
+}
