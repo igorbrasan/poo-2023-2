f24bf733f6300bad73c5a61d1c2621af88f5a979
igor <igorbraga.rsantos@gmail.com>
2023-09-07 11:58:49 -0300

    Go

--- lista01 - Automovel/src/Main.java
+++ lista01 - Automovel/src/Main.java
@@ -3,6 +3,11 @@
 public class Main {
     public static void main(String[] args) {
         Automovel automovel = new Automovel("gasolina", "ford", "ka");
+        System.out.printf("Setando combustível como álcool");
+        automovel.setCombustivel("alcool");
+        automovel.combustivel();
+        automovel.setCombustivel("flex");
+        automovel.combustivel();
 
         System.out.printf("Hello and welcome!");
         }
