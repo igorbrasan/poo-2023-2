e6bcb8982e3e1bd4ea40f51397cd1f6240409aca
Igor Reis <igorbraga.rsantos@gmail.com>
2023-09-28 00:12:06 -0300

    Exercício finalizado

--- Exercicio_Fixacao_4_Livraria/src/main/java/org/example/Main.java
+++ Exercicio_Fixacao_4_Livraria/src/main/java/org/example/Main.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
@@ -20,25 +21,107 @@ public class Main {
     }
 }
 
+/**
+ * Classe para representar a livraria(estoque de livros)
+ */
 class Livraria{
     private String nome, cnpj;
     private ArrayList<Livro> livros;
 
+    /**
+     * Construtor vazio
+     */
     public Livraria() {
     }
 
-    public boolean Comprarlivro(Livro livro, int quantidade){
+    /**
+     *Registrar a compra de exemplares de um livro. Neste caso é passado o título do
+     * livro e a quantidade comprada. Caso o livro esteja na lista, a qtd comprada é
+     * adicionada a qtd em estoque e retorna-se true. Caso o livro não seja encontrado,
+     * retorne-se false.
+     * @param titulo
+     * @param quantidade
+     * @return
+     */
+    public boolean Comprarlivro(String titulo, int quantidade){
+        for (Iterator <Livro> it = livros.iterator(); it.hasNext() ;) {
+            Livro elem = it.next();
+            if(elem.getTitulo().equals(titulo)){
+                elem.addtoEstoque(quantidade);
+                return true;
+            }else{
+                return false;
+            }
+        }
 
-        return true;
     }
+
+    /**
+     * Vendedores podem consultar informações dos livros no momento da venda. É
+     * passado o nome do vendedor e título do livro. Quando o livro é encontrado na
+     * lista retorna-se uma mensagem com o título, preço e qtd em estoque. Caso a
+     * quantidade de estoque seja zero, é enviada a seguinte mensagem : " Sem estoque,
+     * deseja fazer pedido de novos exemplares?". Caso o livro não conste na lista a
+     * mensagem de retorno é " Título não faz parte do nosso catálogo".
+     * @param Vendedor
+     * @param titulo
+     * @return
+     */
+    public String consultar(String Vendedor, String titulo){
+        for (Iterator <Livro> it = livros.iterator(); it.hasNext() ;) {
+            Livro elem = it.next();
+            if(elem.getTitulo().equals(titulo)){
+                if(elem.getEstoque() == 0){
+                    return("Sem estoque. Deseja fazer pedido de novos exemplares?");
+                }else {
+                    return (elem.getTitulo() + elem.getPreco() + elem.getEstoque());
+                }
+            } else {
+                return ("Título não faz parte do nosso catálogo");
+            }
+        }
+    }
+
+    /**
+     * Mensalmente o sistema deve emitir uma listagem com todas as informações dos
+     * livros e um total da quantidade de todos os livros que possuem estoque.
+     * @return
+     */
+    public String relatorioMensal(){
+        System.out.println("Relatório mensal de estoque:");
+        int quantidade = 0;
+        for(Iterator<Livro>it = livros.iterator(); it.hasNext();){
+            Livro elem = it.next();
+            quantidade = quantidade + elem.getEstoque();
+            System.out.println("Título = "+ elem.getTitulo());
+            System.out.println("Autor = "+ elem.getAutorPrincipal());
+            System.out.println("Área = "+ elem.getArea());
+            System.out.println("Estoque = "+ elem.getEstoque());
+            System.out.println("Preço = "+ elem.getPreco());
+        }
+        System.out.println("Estoque total de livros = " + quantidade);
+
+        return ("Relatório Finalizado");
+    }
+
 }
 
+/**
+ * Classe Livro
+ */
 class Livro {
     private String titulo, autorPrincipal, area;
-    private boolean preco;
+    private double preco;
     private int estoque;
 
-    public Livro(String titulo, String autorPrincipal, String area, boolean preco) {
+    /**
+     * Construtor de livro geral, com estoque zero.
+     * @param titulo
+     * @param autorPrincipal
+     * @param area
+     * @param preco
+     */
+    public Livro(String titulo, String autorPrincipal, String area, double preco) {
         this.titulo = titulo;
         this.autorPrincipal = autorPrincipal;
         this.area = area;
@@ -46,12 +129,100 @@ class Livro {
         this.estoque = 0;
     }
 
-    public Livro(String titulo, String autorPrincipal, boolean preco) {
+    /**
+     * Construtor simplificado, com área fixada em Multidisciplinar.
+     * @param titulo
+     * @param autorPrincipal
+     * @param preco
+     */
+    public Livro(String titulo, String autorPrincipal, double preco) {
         this.titulo = titulo;
         this.autorPrincipal = autorPrincipal;
         this.area = "Multidisciplinar";
         this.preco = preco;
         this.estoque = 0;
+    }
+
+
+
+    /**
+     * Getters
+     * @return
+     */
+    public String getTitulo() {
+        return titulo;
+    }
+
+    /**
+     * Setter
+     * @param titulo
+     */
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public String getAutorPrincipal() {
+        return autorPrincipal;
+    }
+
+    /**
+     *
+     * @param autorPrincipal
+     */
+    public void setAutorPrincipal(String autorPrincipal) {
+        this.autorPrincipal = autorPrincipal;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public String getArea() {
+        return area;
+    }
+
+    /**
+     *
+     * @param area
+     */
+    public void setArea(String area) {
+        this.area = area;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public double getPreco() {
+        return preco;
+    }
+
+    /**
+     *
+     * @param preco
+     */
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public int getEstoque() {
+        return estoque;
+    }
+
+    /**
+     * Incluir livros no estoque
+     * @param quantidade
+     */
+    public void addtoEstoque(int quantidade) {
+        this.estoque = this.estoque + quantidade;
     }
 }
 
